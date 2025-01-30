ff52a5fc66123e808d3616fbed8904ca5b1899a4
JDGarciag12 <jdgarciag12uniandes.edu.co>
2025-01-30 15:09:38 -0500

    cambios del Estudiante 3

--- App/logic.py
+++ App/logic.py
@@ -135,7 +135,7 @@ def load_books_tags(catalog, filename):
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
     booktagfile = os.path.join(data_dir, filename)
     input_file = csv.DictReader(open(booktagfile, encoding="utf-8"))
-    catalog = create_book_tag_file(catalog)
+    catalog = create_book_tag_list(catalog)
     for booktag in input_file:
         add_book_tag(catalog, booktag)
     return book_tag_size(catalog)
