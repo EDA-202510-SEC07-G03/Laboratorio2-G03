cff8a2c9d4462fee20df58c0f138342384040ce7
t.aponte <t.aponte@uniandes.edu.co>
2025-02-05 00:17:13 -0500

    cambios en main

--- App/view.py
+++ App/view.py
@@ -72,7 +72,7 @@ def load_books(app):
     :type app: logic
     """
     books = logic.load_books(app,
-                             "GoodReads/books-small.csv")
+                             "GoodReads/books.csv")
     return books
 
 
@@ -97,7 +97,7 @@ def load_books_tags(app):
     :param app: Aplicación de la lógica
     :type app: logic
     """
-    book_tags= logic.load_books_tags(app,"GoodReads/book_tags-small.csv")
+    book_tags= logic.load_books_tags(app,"GoodReads/book_tags.csv")
 
 
     return book_tags
